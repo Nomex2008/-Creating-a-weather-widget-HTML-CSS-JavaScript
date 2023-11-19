1440bb4acb163587849ba344009b07b30a687389
Alex <97978327+Nomex2008@users.noreply.github.com>
2023-11-19 23:44:02 +0200

    Commit 4
    Add server and server info to project/widjet.

--- js/mainJS.js
+++ js/mainJS.js
@@ -12,6 +12,55 @@ async function loadWeather(e) {
         <div class="weather__loading">
             <img src="img/loading.gif" alt="loading">
         </div>`;
+
+    const server = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=Rivne&appid=c8c900c0763fb7da7adb907585640492'
+    //console.log(server)
+
+    const response = await fetch(server, {
+        method:'GET',
+    });
+    //console.log(response)
+
+    const responseResult = await response.json();
+    //console.log(responseResult)
+
+    if (response.ok) {
+        getWeather(responseResult)
+    } else {
+        weatherBlock.innerHTML = responseResult.massage;
+    }
+}
+
+function getWeather(data) {
+    //console.log(data)
+
+    const location = data.name;
+    const temp = Math.round(data.main.temp);
+    const feels_like = Math.round(data.main.feels_like);
+    const weatherStatus = data.weather[0].main;
+    const weatherIcon= data.weather[0].icon;
+    //console.log([location,temp,feels_like,weatherStatus,weatherIcon])
+
+    const templayt = `
+        <div class="weather__header">
+
+            <div class="weather__main">
+                <div class="weather__city">${location}</div>
+                <div class="weather__status">${weatherStatus}</div>
+            </div>
+
+            <div class="weather__icon">
+                <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Clouds">
+            </div>
+
+        </div>
+
+        <div class="weather__temp">${temp}</div>
+
+        <div class="weather__feels-like">Feels like: ${feels_like}</div>`
+    //console.log(templayt)
+
+    weatherBlock.innerHTML = templayt;
 }
 
 if(weatherBlock) {
